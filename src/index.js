648c6f4d7dcb610a8c652cb20c1c6bd5bba4b5fe
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2020-04-21 01:08:52 -0300

    Código refeito

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 'use strict'
 
-const limit = 15; // regra: números de série até passar de 350
-
 const fibonacci = () => {
-    return Array.from({ length: limit })
-		.reduce((acc, _val, i) => 
-            acc.concat(i > 1 ? acc[i - 1] + acc[i - 2] : i),[]);
+    const limit = 350; // regra: números de série até passar de 350
+    const array = [0,1]; 
+    while(array[array.length - 1] <= limit){ // 1 menor ou igual a 350 ?
+        array.push(array[array.length - 2] + array[array.length - 1]); // 1 + 0 = 1... 1 + 1 = 2 .. 1 + 2 = 3 .. 2 + 3 = 5 ..
+    }
+    return array;
 }
 
-const isFibonnaci = (num) => fibonacci().includes(num);
+const isFibonnaci = (num) => fibonacci().includes(num); // num está dentro da lista ? sim : não
 
 module.exports = {
     fibonacci,
